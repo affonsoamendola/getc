b342209412f240f7cc65c7c4f2546aeb119797ef
Affonso Amendola <affonsoamendola@bol.com.br>
2018-10-08 16:31:07 -0300

    Added initial formula from ESO's formula book for number of counts detected by spectral resolution element

--- gmt-etc.py
+++ gmt-etc.py
@@ -24,6 +24,20 @@ class Field:
 
 				self.flux[i][j] = height * np.exp( (-4.) * np.log(2.) * ((float(i)-posX)**2.+(float(j)-posY)**2.)  / fwhm**2.  )
 
+OBSERVATION_MODE_IMAGING = 0
+OBSERVATION_MODE_SPECTROSCOPY = 1
+
+SOURCE_TYPE_POINT = 0
+SOURCE_TYPE_EXTENDED = 1
+
+def count_per_res(	observation_mode, source_type, incident_flux, filter_band_width, 
+				  	exposure_time, efficiency, telescope_surface, photon_energy):	
+	if(observation_mode == OBSERVATION_MODE_IMAGING):
+		if(source_type == SOURCE_TYPE_POINT):
+			N = (incident_flux*filter_band_width*exposure_time*efficiency*telescope_surface)/photon_energy
+			return N
+	#elif(observation_mode == OBSERVATION_MODE_SPECTROSCOPY):
+
 def out_matrix_file(filename, matrix):
 	f = open(filename, "w")
 	for i in range(len(matrix)):
@@ -31,7 +45,6 @@ def out_matrix_file(filename, matrix):
 			f.write(str(matrix[i][j]))
 			f.write(" ")
 		f.write("\n")
-
 
 field = Field(0.1, 10, 10)
 field.make_gaussian(10.,10.,1.,100.)
